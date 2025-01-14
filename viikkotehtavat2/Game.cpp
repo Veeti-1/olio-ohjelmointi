d7c7a98abc45be361127c709901324adf494869d
Veeti-1 <veeti.salomaa155@gmail.com>
2025-01-14 14:55:59 +0200

    valmiit tehtavat

---
+++ viikkotehtavat2/Game.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <ctime>
+
+#include "Game.h"
+
+using namespace std;
+
+Game::Game(int ylaraja)
+{
+    maxNumber = ylaraja;
+    std::srand(std::time(NULL));
+    randomNumber = (rand()%maxNumber) +1 ;
+
+
+}
+
+void Game::play(){
+    numOfGuesses = 0;
+    cout << "arvaa oikea luku valilta 1-" << maxNumber << ": ";
+    while(true){
+        cin >> playerGuess;
+        if(playerGuess==randomNumber){
+            cout << "arvasit oikein!" << endl;
+
+            numOfGuesses++;
+            printGameResult();
+            break;
+        }
+        else if(playerGuess<randomNumber){
+            cout << "annettu luku on pienempi kuin arvottuluku, yrita uudestaan: ";
+            numOfGuesses++;
+        }
+        else{
+            cout << "annettuluku on suurempi kuin arvottuluku, yrita uudestaan: ";
+            numOfGuesses++;
+        }
+    }
+}
+
+void Game::printGameResult(){
+    cout << "arvottu luku oli: " << randomNumber << endl;
+    cout << " arvausten maara: "<< numOfGuesses << endl;
+
+}
+
+Game::~Game(){}
--- viikkotehtavat2/main.cpp
+++ viikkotehtavat2/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
+
+#include "Game.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int ylaraja;
+    cout << "anna korkein etsittava luku: ";
+    cin >> ylaraja;
+    Game game(ylaraja);
+
+    game.play();
+
+
     return 0;
 }
