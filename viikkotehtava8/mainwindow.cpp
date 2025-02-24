4263bdf795fd5cd8a609d56b2691b3fed33e38e1
Veeti-1 <veeti.salomaa155@gmail.com>
2025-02-24 19:59:52 +0200

    valmis

--- viikkotehtava8/mainwindow.cpp
+++ viikkotehtava8/mainwindow.cpp
@@ -10,18 +10,18 @@ MainWindow::MainWindow(QWidget *parent)
 
     ui->aika1 -> setRange(0, 100); //asetetaan ranget 0-100
     ui->aika2->setRange(0,100);
-    ui->aika1->setValue(0);
+    ui->aika1->setValue(0);//nollataan aluksi progressbaarit
     ui->aika2->setValue(0);
       connect(timer, &QTimer::timeout, this, &MainWindow::timeout);
     setGameInfoText("select playtime and startgame",15);
 
 }
 void MainWindow::timeout(){
-    if(playerturn && player1Time>0){
-        player1Time--;
+    if(playerturn && player1Time>0){//jos on pelaajan 1 vuoro ja peliaikaa on vielä jäljellä
+        player1Time--; //vähennetään pelaajan 1 ajasta 1sec/progresbar mene alaspäin joka sekuntti
     }
-    else if (!playerturn && player2Time > 0){
-        player2Time--;
+    else if (!playerturn && player2Time > 0){ //jos on pelaajan 2 vuoro ja peliaikaa on vielä jäljellä
+        player2Time--;//vähennetään pelaajan 2 ajasta 1sec/progresbar mene alaspäin joka sekuntti
     }
     updateProgressBar();
 }
@@ -35,12 +35,12 @@ void::MainWindow::setGameInfoText(QString text, short fontSize){
     progress2 = (player2Time*100)/gameTime;
      ui->aika1->setValue(progress1);
     ui->aika2->setValue(progress2);
-     if(player1Time == 0){
-        timer->stop();
+     if(player1Time == 0){ //jos pelaajan 1 aika loppuu
+        timer->stop(); //timer pysäyteteään
          setGameInfoText("player 2 won", 15);
      }
-     if(player2Time==0){
-         timer->stop();
+     if(player2Time==0){//jos pelaajan 2 aika loppuu
+         timer->stop();//timer pysäyteteään
          setGameInfoText("player 1 won", 15);
      }
 }
@@ -51,9 +51,9 @@ void MainWindow::on_startgame_clicked()
 
     setGameInfoText("game ongoing",15);
     if(gameTime>0){
-        player1Time = gameTime;
+        player1Time = gameTime; // asetetaan pelaajien 1 ja 2 ajaksi valittu peliaika
         player2Time = gameTime;
-        playerturn = true;
+        playerturn = true; //pelaaja 1 aloittaa
         timer->start(1000);
     }
 
@@ -61,7 +61,7 @@ void MainWindow::on_startgame_clicked()
 
 void MainWindow::on_time1_clicked()
 {
-    gameTime = 120;
+    gameTime = 120; // asetetaan ajaksi 120s
     ui->aika1 -> setValue(100);//asetetaan pelaajan 1 progressbar 100:n
     ui->aika2 -> setValue(100);//asetetaan pelaajan 2 progressbar 100:n
     setGameInfoText("ready to play",15);
@@ -69,7 +69,7 @@ void MainWindow::on_time1_clicked()
 
 void MainWindow::on_time2_clicked()
 {
-    gameTime = 300;
+    gameTime = 300; // asetetaan ajaksi 5min
     ui->aika1 -> setValue(100);//asetetaan pelaajan 1 progressbar 100:n
     ui->aika2 -> setValue(100);//asetetaan pelaajan 2 progressbar 100:n
     setGameInfoText("ready to play",15);
@@ -78,7 +78,7 @@ void MainWindow::on_time2_clicked()
 void MainWindow::on_swtitchplayer_clicked()
 {
     if(playerturn){
-        playerturn = false;
+        playerturn = false; // vaihtaa pelaajaan 2
 
     }
 }
@@ -87,7 +87,7 @@ void MainWindow::on_swtitchplayer_clicked()
 void MainWindow::on_switchplayer2_clicked()
 {
     if(!playerturn){
-        playerturn = true;
+        playerturn = true; //vaihtaa pelaajaan 1
     }
 }
 
@@ -95,10 +95,10 @@ void MainWindow::on_switchplayer2_clicked()
 void MainWindow::on_stopgame_clicked()
 {
 
-    timer->stop();
-    player1Time = 0;
+    timer->stop(); //timer pysäytetään
+    player1Time = 0;//nollataan ajat
     player2Time = 0;
-    ui->aika1->setValue(0);
+    ui->aika1->setValue(0); //nollataan progressbaarit
     ui->aika2->setValue(0);
     setGameInfoText("game over, restart to play",15);
 }
